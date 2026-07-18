46b9a980c04095511545382d57603b0842d425c6
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2026-07-18 14:47:40 +0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComplainManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
